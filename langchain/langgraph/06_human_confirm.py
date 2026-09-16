5fecc74ff615cec08122bb533f87357792f1357f
taekgeun <taekgeun0816@gmail.com>
2026-09-16 14:28:04 +0900

    06_human_confrim(2)
    - break추가 human_confirm
    - checkpoint configurable
    설정

--- langchain/langgraph/06_human_confirm.py
+++ langchain/langgraph/06_human_confirm.py
@@ -1,5 +1,8 @@
 # 1. 저장소 생성
+import uuid
+
 from langchain_ollama import ChatOllama
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.constants import END
 from langgraph.graph import StateGraph
 from pydantic import BaseModel
@@ -43,8 +46,20 @@ wf.add_node('send',send_mail_node)
 wf.set_entry_point('writer')
 wf.add_edge('writer','send')
 wf.add_edge('send', END)
+# 6. 컴파일(저장소, 정지포인트)
+# checkpointer = 어디에 저장할 것인가?
+#interrupt_before = 어떤 노드 전에 세울 것인가?
+memory = MemorySaver()
+app = wf.compile(checkpointer=memory, interrupt_before=['send'])
 
-app = wf.compile()# 6. 컴파일
 # 7. 실행
-result = app.invoke({"title":"전사 야우회 참여 공지 메일"})
-print(result)
+#현재이것을 실행하는 thread id를 지정 (어떤 프로세스인지 식별하기 위해)
+config = {'configurable':{'thread_id':uuid.uuid4()}}
+result = app.invoke({"title":"전사 야우회 참여 공지 메일"}, config)
+yn = input('작성된 초안을 승인하고 발송 하시겠습니까?')
+if yn.lower().strip() == 'y':
+    print('승인 완료 발송 시작')
+    print(f'memory: {memory}')
+    app.invoke(None,config)
+else:
+    print('발송이 거부 되었습니다.')
